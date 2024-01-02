ec3e2076015e7f7be435a971c7362badd6e04555
wwl-lsh <lsh@winwinlab.co.kr>
2024-01-02 11:29:32 +0900

    React-맛보기 예제 추가

---
+++ React-맛보기/src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styles from "./Card.module.scss";
+
+type CardProps = {
+  title: string;
+};
+
+const Card = (props: CardProps) => {
+  const { title } = props;
+
+  return (
+    <div className={styles.cardItem}>
+      <div>{title}</div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ React-맛보기/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className={styles.content}>ⓒ 2023 코드드림. All rights reserved.</div>
+    </footer>
+  );
+};
+
+export default Footer;
