7eb5290ea94ab340a5a70c988b1ecf27509d2e8d
eeesnghyun <szhyun2002@gmail.com>
2024-01-03 20:49:20 +0900

    React-맛보기 useEffect 예제 추가

--- React-맛보기-React-Hooks/src/components/Card/Card.tsx
+++ React-맛보기-React-Hooks/src/components/Card/Card.tsx
@@ -29,7 +29,7 @@ const Card = (props: CardProps) => {
 
   useEffect(() => {
     console.log("Card click count : ", count);
-  });
+  }, [count]);
 
   https: return (
     <div className={styles.cardItem}>
